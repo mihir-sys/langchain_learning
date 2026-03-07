67eee57fba7b05e22f2556c6403bb0d3f480f020
SushreeRani <160645706+SushreeRani@users.noreply.github.com>
2026-03-07 15:53:21 +0400

    changes in main

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import os
-
 from dotenv import load_dotenv
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-load_dotenv()
+load_dotenv(override=True)
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 def main():
     print("Hello, World! from main.py langchain learning")
-    print("Print google api key")
-    api_key = os.environ.get("GEMINI_API_KEY")
-    print(api_key)
 
     # Initialize model  
     llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=api_key
+    google_api_key=GEMINI_API_KEY
     )
+
+    question = "Explain what is Langchain and how it works in 20 words"
+    response = llm.invoke(question)
+    print("Response:",  response.content)
+
 if __name__ == "__main__":
     main()
